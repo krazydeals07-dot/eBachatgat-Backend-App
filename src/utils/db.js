e1c5da7d23df6851bfe4bf233746a06ac5ec4bb3
krazydeals07-dot <krazydeals07@gmail.com>
2025-10-04 16:32:39 +0000

    debug

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,11 +4,12 @@ const logger = require('../../logger');
 
 const connectDB = async () => {
   try {
+    logger.info(`Connecting to MongoDB - ${process.env.MONGODB_URI}`);
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    logger.info('MongoDB connected successfully');
+    logger.info('MongoDB connected successfully - ');
   } catch (error) {
     logger.critical('MongoDB connection error:', error);
     process.exit(1);
